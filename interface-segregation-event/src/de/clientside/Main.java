4d8a84fb031920fbfd10417a0bc6d757574c6b08
Jo Wagner <jowagner@limago.de>
2025-05-12 08:11:31 +0200

    initial commit

---
+++ interface-segregation-event/src/de/clientside/Main.java
@@ -0,0 +1,31 @@
+package de.clientside;
+
+
+import de.tiere.Schwein;
+
+public class Main {
+
+
+	public static void main(String[] args) {
+
+		new Main().run();
+	}
+
+	private void run() {
+		Schwein schwein = new Schwein("Miss Piggy");
+
+		for (int i = 0; i < 11; i++) {
+			schwein.fuettern();
+		}
+
+
+	}
+
+
+
+}
+
+
+
+
+
